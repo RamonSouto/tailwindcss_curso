7478eb9eb8b80190f1218e6ed8a3166616c383cb
Ramon Souto <rdssi@hotmail.com>
2021-12-30 14:13:31 -0300

    Capitulo 6 realizado com sucesso passado customização do TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const {colors} = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
-        extend: {}
+        screens:{
+            'tablet' : '640px',
+            'cel':{'max':'400px'},
+        },
+        extend: {
+            colors:{
+                'new-blue': '#243C5A',
+                gray: {
+                    ...colors.gray,
+                    '900': '#999'
+                }
+            },
+            spacing:{
+                '50' : '20rem',
+            }
+        }
     },
     plugins: []
 }
