dd5eff528dc29051ee299655f9a7514ce53a0325
Ramon David Souto <rdssi@hotmail.com>
2022-01-21 23:23:52 -0300

    Capitulo 14 - Realizado ate a aula 5 que contempla o menu superior.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,35 +2,24 @@ module.exports = {
     content: ['./src/**/*.{html,js}'],
     theme: {
         extend: {
-            spacing: {
-                14: '3.5rem',
-                '125px': '125px'
-            },
-            colors: {
-                hoverspt: '#18D760',
-                'purple-main': '#2D46B9',
-                'green-main': '#1ED760'
-            },
-            backgroundImage: theme => ({
-                'spotify-theme': 'url("../img/bursts.svg")',
-                'spotify-theme-mobile': 'url("../img/bursts-mobile.svg")',
-                cidade: 'url("../img/bg_cidade.jpg")',
-                tailwind: 'url(".../img/tailwind.png")'
-            }),
-            backgroundSize: {
-                '175%': '175%',
-                '195%': '195%'
-            },
-            backgroundPosition: {
-                banner: '46% 4%',
-                'banner-mobile': 'top 25% center'
-            },
-            fontSize: {
-                '9xl': '9rem',
-                '12xl': '8rem'
-            },
             fontFamily: {
                 arial: 'Arial'
+            },
+            colors: {
+                'darkblue-500': '#485769',
+                'darkblue-600': '#37475A',
+                'darkblue-700': '#232F3E',
+                'darkblue-900': '#191E26',
+                'darkblue-950': '#131A22',
+                'gold-100': '#FEBD69',
+                'gold-200': '#FAA63A',
+                'orange-650': '#EA7911',
+                yellow: '#F0C48'
+            },
+            spacing: {
+                7: '1.75rem',
+                '75px': '75px',
+                '210px': '210px'
             }
         }
     },
